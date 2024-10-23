c9abd3d0b2e52f861b666c2b8c9dfb2bad71dc6a
Aditya Bansal <adityabansal114@gmail.com>
2024-10-23 17:58:39 +0530

    first commit and auth

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from "./db/connectDB.js";
+import authRoutes from "./routes/authRoutes.js"
+import cookieParser from 'cookie-parser';
+dotenv.config();
+
+const PORT= process.env.PORT;
+const app= express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth",authRoutes);
+
+app.listen(PORT, async()=>{
+    connectDB();
+    console.log(`server is running on PORT : ${PORT}`);
+})
+
