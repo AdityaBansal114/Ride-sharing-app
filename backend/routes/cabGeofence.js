0d6444ef7140b21556549b6b8c843ff8777596c6
Aditya Bansal <adityabansal114@gmail.com>
2024-10-24 04:47:10 +0530

    validate user

--- backend/routes/cabGeofence.js
+++ backend/routes/cabGeofence.js
@@ -1,7 +1,10 @@
 import express from "express";
+import { getDistance } from "../controllers/cab_geofence/getDistance.js";
+import validateUser from "../middlewares/validateUser.js";
 const router = express.Router();
 
-router.post("/getLocation", )
+
+router.post("/getLocation", validateUser , getDistance )
 
 //TODO
 // check if the user is logged in
--- backend/routes/tripRoutes.js
+++ backend/routes/tripRoutes.js
@@ -2,11 +2,13 @@ import express from "express"
 import createTrip from "../controllers/trip/createTrip.js";
 import findTrip from "../controllers/trip/findTrips.js";
 import { joinRide } from "../controllers/trip/joinRide.js";
+import validateUser from "../middlewares/validateUser.js";
+
 const router = express.Router();
 
-router.post("/create",createTrip);  
-router.post("/find",findTrip);
-router.post("/join", joinRide);
+router.post("/create",validateUser, createTrip);  
+router.post("/find",validateUser,findTrip);
+router.post("/join", validateUser, joinRide);
 
 //TODO
 // check if the user is logged in
